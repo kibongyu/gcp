6f495f224faba089ef4e74dc544ae624b484b11c
loadbong <115207570+kibongyu@users.noreply.github.com>
2024-07-24 18:00:22 +0900

    commit

--- main.tf
+++ main.tf
@@ -16,17 +16,26 @@ variable "gcp_credentials" {
 # 로컬 변수
 locals {
   project = "cloudexperience"
-  env     = "wgpark"
+  env     = "cscontents"
   stage   = "dev"
   name    = "${local.project}-${local.env}-${local.stage}"
-  region  = "asia-northeast3"
+  region  = "us-west1"
+}
+
+# 모듈 변수
+module "cscontents_module" {
+  source = "../00_structure"
+  gcpSubnetPub = ["10.0.0.0/24","10.0.1.0/24"]
+  gcpSubnetPri = ["10.0.2.0/24","10.0.3.0/24"]
+  gcp-az = ["a","b"]
+  gcp-sshkey ="dlsrk489"
 }
 
 provider "google" {
   project = "teak-flash-430301-c8"
   credentials = var.gcp_credentials
-  region = "europe-west1"
-  zone = "europe-west1-a"
+  region = "us-west1"
+  zone = "us-west1-a"
 }
 
 resource "google_compute_network" "vpc_network" {
